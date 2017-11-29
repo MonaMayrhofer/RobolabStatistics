9a7b84b34a990292cc87fb77d195025895a7eafc
Erik Mayrhofer <obyoxar@gmail.com>
2017-11-29 21:22:56 +0100

    Wipped General Buildscript
    + Fixed Library.path
    + Moved some code to the general buildscript

--- training/Mayrhofer_Gradle05_BigProject/src/main/java/App.java
+++ training/Mayrhofer_Gradle05_BigProject/src/main/java/App.java
@@ -1,3 +1,4 @@
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
@@ -10,6 +11,7 @@ public class App {
     }
 
     public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println(new App().getGreeting());
 
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
