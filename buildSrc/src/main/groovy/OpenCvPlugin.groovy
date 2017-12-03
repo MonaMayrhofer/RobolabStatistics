e83168b4f328057d205926102367939ff8c07592
Obyoxar <obyoxar@gmail.com>
2017-12-03 21:08:12 +0100

    Adapted Opencv-Plugin
    + opencv plugin now adds correct library paths on non-unix systems

--- buildSrc/src/main/groovy/OpenCvPlugin.groovy
+++ buildSrc/src/main/groovy/OpenCvPlugin.groovy
@@ -1,3 +1,4 @@
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.JavaExec
@@ -7,7 +8,18 @@ class OpenCvPlugin implements Plugin<Project> {
     @Override
     void apply(Project target) {
         target.tasks.withType(JavaExec) {
-            systemProperty "java.library.path", target.rootProject.file('lib/opencv').absolutePath
+            def libFolder
+            if(Os.isFamily(Os.FAMILY_WINDOWS)){
+                if(Os.isArch("amd64")){
+                    libFolder = target.rootProject.file('lib/opencv/x64')
+                }else{
+                    //Untested
+                    libFolder = target.rootProject.file('lib/opencv/x86')
+                }
+            }else{
+                libFolder = target.rootProject.file('lib/opencv')
+            }
+            systemProperty "java.library.path",libFolder.absolutePath
         }
 
         target.dependencies {
