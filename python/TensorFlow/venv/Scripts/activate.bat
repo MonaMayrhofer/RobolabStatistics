24e914844a07d1f2ce965ce72b1176faecaeb1a0
Florian Schwarcz <florian.schwarcz@gmx.at>
2018-01-01 14:25:22 +0100

    Renewed TensorFlow project to install TensorFlow

--- python/TensorFlow/venv/Scripts/activate.bat
+++ python/TensorFlow/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=D:\Dokumente\Schule\SYP\RoboLabStatistics\python\TensorFlow\venv"
+set "VIRTUAL_ENV=C:\Users\Admin\Documents\Schule\SYP\RobolabStatistics\python\Tensorflow\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
