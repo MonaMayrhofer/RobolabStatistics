d5cae87701db4e2dba7629c2efd0c59ab283cdfa
Erik Mayrhofer <obyoxar@gmail.com>
2018-01-22 11:11:35 +0100

    Redone Face-Detection

--- apps/facedetector/snippets.py
+++ apps/facedetector/snippets.py
@@ -36,11 +36,10 @@ paused = True
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces, rejectLevels, levelWeights = face_cascades.detectMultiScale3(gray, 1.3, 5, 0, minSize, maxSize, True)
 
-directX = random.uniform(-0.9, 0.9)
-direction = (directX, (1-directX) ** 0.5)
+directY = random.uniform(-0.9, 0.9)
+direction = ((1-directY) ** 0.5, directY)
 speed = 3
 ballPos = (img.shape[1]/2, img.shape[0]/2)
-
 
 timeout = 10
 while True:
@@ -48,10 +47,29 @@ while True:
     cv2.flip(img,1,img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     oldFaces = faces
-    faces, rejectLevels, levelWeights = face_cascades.detectMultiScale3(gray, 1.3, 5, 0, minSize, maxSize, True)
+
+    fieldsize = int(img.shape[1] / 3)
+    # faces, rejectLevels, levelWeights = face_cascades.detectMultiScale3(gray, 1.3, 5, 0, minSize, maxSize, True)
+
+    facesLeft, rejectLevelsLeft, levelWeightsLeft = face_cascades.detectMultiScale3(gray[0:img.shape[0], 0:fieldsize], 1.3, 5, 0, minSize, maxSize, True)
+    facesRight, rejectLevelsRight, levelWeightsRight = face_cascades.detectMultiScale3(gray[0:img.shape[0], 2*fieldsize:3*fieldsize], 1.3, 5, 0, minSize, maxSize, True)
+
+    # TODO Wichtigstes gesicht ermitteln
+
+
+    if len(facesLeft) != 0 and len(facesRight) != 0:
+        leftInd = np.argmax(levelWeightsLeft)
+        rightInd = np.argmax(levelWeightsRight)
+
+        faces = [facesLeft[leftInd], facesRight[rightInd]]
+        faces[1][0] += 2*fieldsize
+
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0))
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+
+
 
     if len(faces) != 2:
         if timeout > 0:
@@ -80,12 +98,12 @@ while True:
         z2 = x2 + w2
         t2 = y2 + h2
         if (ballPos[0] - 20 < z1 and t1 > ballPos[1] > y1 and direction[0] < 0 and ballPos[
-            0] + 40 > z1) \
+            0] + w1 / 2 > z1) \
                 or (ballPos[0] + 20 > x2 and t2 > ballPos[1] > y2 and direction[0] > 0 \
-                    and ballPos[0] - 40 < z2):
+                    and ballPos[0] - w2/ 2 < z2):
             direction = (-direction[0] + random.uniform(-0.1, 0.1), direction[1] + random.uniform(-0.1, 0.1))
         if (ballPos[0] + 20 > img.shape[1] and direction[0] > 0) or (ballPos[0] < 20 and direction[0] < 0):
-            directX = random.uniform(-0., 0.9)
+            directX = random.uniform(-0.9, 0.9)
             direction = (directX, (1 - directX) ** 0.5)
             speed = 3
             ballPos = (img.shape[1] / 2, img.shape[0] / 2)
@@ -95,9 +113,12 @@ while True:
     realBallPos = (int(ballPos[0]), int(ballPos[1]))
     cv2.circle(img, realBallPos, 20, (0, 0, 255), 5)
     cv2.circle(img, realBallPos, 10, (255, 0, 0), 5)
-    cv2.line(img, (int(img.shape[1] / 3), 0), (int(img.shape[1] / 3), img.shape[0]), (0, 0, 0))
-    cv2.line(img, (int(img.shape[1] / 3 * 2), 0), (int(img.shape[1] / 3 * 2), img.shape[0]), (0, 0, 0))
-    speed *= 1.005
+    cv2.line(img, (int(img.shape[1] / 3), 0), (int(img.shape[1] / 3), img.shape[0]), (0, 0, 0), 2)
+    cv2.line(img, (int(img.shape[1] / 3 * 2), 0), (int(img.shape[1] / 3 * 2), img.shape[0]), (0, 0, 0), 2)
+    if speed < 10:
+        speed *= 1.005
+
+    cv2.putText(img, "Paused: {} {}".format(paused, timeout), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
     cv2.imshow('img', img)
 
     k = cv2.waitKey(30) & 0xff
