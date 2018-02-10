a870e915a1ac69b50f80a70f48a5b84086f724e5
Erik Mayrhofer <obyoxar@gmail.com>
2018-02-10 20:38:55 +0100

    Added File-Saving

--- apps/training_tf_simple_m/keras_advanced.py
+++ apps/training_tf_simple_m/keras_advanced.py
@@ -7,9 +7,12 @@ from robolib.robogui import pixel_editor
 import cv2
 import keras
 import numpy as np
+import os.path as path
 
 
 DEBUG = False
+RELEARN = False
+MODEL_FILENAME = "KerasAdvancedModel"
 
 # DATA STUFF
 label_labels = ["Horizontal", "Vertikal"]
@@ -38,24 +41,33 @@ for la, d in zip(labels, data):
 
 # MACHINE LEARNING STUFF
 
-model = Sequential()
-model.add(Conv2D(20, (3, 3), activation='relu', input_shape=(size, size, 1)))
-model.add(Conv2D(10, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(size*size, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(2, activation='relu'))
-model.add(Dense(2, activation='softmax'))
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model = None
+if RELEARN or not path.isfile(MODEL_FILENAME):
+    print("Model will be recreated: File {} exists: {}".format(MODEL_FILENAME, path.isfile(MODEL_FILENAME)))
+    model = Sequential()
+    model.add(Conv2D(30, (3, 3), activation='relu', input_shape=(size, size, 1)))
+    model.add(Conv2D(20, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(size*size, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(2, activation='relu'))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+else:
+    print("Loading model from File {}".format(MODEL_FILENAME))
+    model = keras.models.load_model(MODEL_FILENAME)
 
 one_hot_labels = keras.utils.to_categorical(labels, num_classes=2)
 model.fit(data, one_hot_labels, epochs=300, batch_size=100)
 
+print("Saving model to {}".format(MODEL_FILENAME))
+model.save(MODEL_FILENAME)
+
 while True:
-    predict_data = [pixel_editor.get_pixel_input(8, 8)]
+    predict_data = [pixel_editor.get_pixel_input(size, size)]
     if all(1.0 not in row for row in predict_data):
         break
 
