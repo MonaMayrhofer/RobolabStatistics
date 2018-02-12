5373bf790f63c0ab82158888b37a63615bc7ea20
ZekReshi <florian.schwarcz@gmx.at>
2018-02-12 09:01:57 +0100

    XO Cnn

--- apps/training_tf_simple_m/XOCnn.py
+++ apps/training_tf_simple_m/XOCnn.py
@@ -1,8 +1,9 @@
 import numpy as np
 import keras
+import robolib.robogui.pixel_editor as pe
 import cv2
 
-DEBUG = False
+DEBUG = True
 
 label_labels = ["X", "O"]
 labels = np.random.randint(0, 2, size=(1000, 1))
@@ -14,13 +15,16 @@ for la, d in zip(labels, data):
     img.fill(-1)
 
     if la == 0:
-        cv2.ellipse(img, (4, 4), (np.random.randint(1, 10), np.random.randint(1, 10)), 0, 0, 1)
+        cv2.ellipse(img, (4, 4), (np.random.randint(1, 5), np.random.randint(1, 5)), 0, 360, 0, 1)
     else:
-        randPointStart = (0, np.random.randint(0, 16))
-        randPointEnd = (0, np.random.randint(0, 16))
-        cv2.line(img, (int(randPointStart / 4), randPointStart % 4), 8 - (int(randPointEnd / 4), 8 - randPointEnd % 4))
-        randPointStart = (0, np.random.randint(0, 16))
-        randPointEnd = (0, np.random.randint(0, 16))
-        cv2.line(img, (8 - int(randPointStart / 4), randPointStart % 4), (int(randPointEnd / 4), 8 - randPointEnd % 4))
+        randPointStart = np.random.randint(0, 16)
+        randPointEnd = np.random.randint(0, 16)
+        cv2.line(img, (int(randPointStart / 4), randPointStart % 4), (8 - int(randPointEnd / 4), 8 - randPointEnd % 4), 1)
+        randPointStart = np.random.randint(0, 16)
+        randPointEnd = np.random.randint(0, 16)
+        cv2.line(img, (8 - int(randPointStart / 4), randPointStart % 4), (int(randPointEnd / 4), 8 - randPointEnd % 4), 1)
 
     d[:, :, :] = np.reshape(img, (size, size, 1))
+
+    if DEBUG:
+        pe.show_image(img)
