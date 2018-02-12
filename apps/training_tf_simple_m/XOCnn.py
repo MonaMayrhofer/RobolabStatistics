037dd01e7de66b6a9a6933a2fdfbc9f725df26c8
ZekReshi <florian.schwarcz@gmx.at>
2018-02-12 09:49:19 +0100

    Fixed Pflichtenheft and worked on XOCnn

--- apps/training_tf_simple_m/XOCnn.py
+++ apps/training_tf_simple_m/XOCnn.py
@@ -1,11 +1,16 @@
 import numpy as np
 import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
 import robolib.robogui.pixel_editor as pe
 import cv2
 
-DEBUG = True
+DEBUG = False
 
-label_labels = ["X", "O"]
+label_labels = ["O", "X"]
 labels = np.random.randint(0, 2, size=(1000, 1))
 size = 9
 data = np.zeros(shape=(1000, size, size, 1))
@@ -15,7 +20,7 @@ for la, d in zip(labels, data):
     img.fill(-1)
 
     if la == 0:
-        cv2.ellipse(img, (4, 4), (np.random.randint(1, 5), np.random.randint(1, 5)), 0, 360, 0, 1)
+        cv2.ellipse(img, (4, 4), (np.random.randint(1, 4), np.random.randint(1, 4)), 0, 360, 0, 1)
     else:
         randPointStart = np.random.randint(0, 16)
         randPointEnd = np.random.randint(0, 16)
@@ -27,4 +32,36 @@ for la, d in zip(labels, data):
     d[:, :, :] = np.reshape(img, (size, size, 1))
 
     if DEBUG:
-        pe.show_image(img)
+        if pe.show_image(img):
+            break
+
+model = Sequential()
+model.add(Conv2D(9, (3, 3), activation='relu', input_shape=(size, size, 1)))
+model.add(Conv2D(9, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), (2, 2)))
+# model.add(Conv2D(3, (3, 3), activation='relu'))
+# model.add(MaxPooling2D((2, 2), (2, 2)))
+model.add(Flatten())
+model.add(Dense(9, activation='relu'))
+model.add(Dense(2, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+one_hot_labels = keras.utils.to_categorical(labels, num_classes=2)
+model.fit(data, one_hot_labels, epochs=300, batch_size=100)
+
+while True:
+    predict_data = [pe.get_pixel_input(size, size)]
+    if all(1.0 not in row for row in predict_data):
+        break
+
+    if DEBUG:
+        print(predict_data)
+    output = model.predict(np.array(predict_data), 1, 3)
+    if all(all(n < 0.9 for n in m) for m in output):
+        print("Don't know, will guess: ")
+    print(label_labels[np.argmax(output)])
+    if DEBUG:
+        print(np.around(output, 5))
