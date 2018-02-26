5f5cddf83ff5aab3c210705148a4bf2886ee9b56
ZekReshi <florian.schwarcz@gmx.at>
2018-02-26 16:57:55 +0100

    Worked on main face detection

--- apps/training_tf_simple_m/HairClassifier.py
+++ apps/training_tf_simple_m/HairClassifier.py
@@ -3,20 +3,24 @@ import numpy as np
 import robolib.modelmanager.downloader as downloader
 
 MODEL_FILE = 'FrontalFace.xml'
-downloader.get_model(downloader.HAARCASCADE_FRONTALFACE_ALT, MODEL_FILE, True)
+downloader.get_model(downloader.HAARCASCADE_FRONTALFACE_ALT, MODEL_FILE, False)
 face_cascades = cv2.CascadeClassifier(MODEL_FILE)
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('img')
 
 while True:
     ret, img = cap.read()
-    """gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces, rejectLevels, levelWeights = face_cascades.detectMultiScale3(gray, 1.3, 5, 0, (40, 40), (100, 100), True)
+    for i in range(len(faces)):
+        x, y, w, h = faces[i]
+        face = img[y-10:y+h+11, x-10:x+w+11]
+        if i == 0:
+            resImg = cv2.resize(face, dst=None, dsize=(128, 128), interpolation=cv2.INTER_LINEAR)
+        else:
+            resImg = cv2.hconcat([cv2.resize(face, dst=None, dsize=(128, 128), interpolation=cv2.INTER_LINEAR), resImg])
     if len(faces) > 0:
-        x, y, w, h = faces[np.argmax(levelWeights)]
-        face = img[y:y+h+1, x:x+w+1]
-        resImg = cv2.resize(face, dst=None, dsize=(64, 64), interpolation=cv2.INTER_LINEAR)
         cv2.imshow('img', resImg)
-        cv2.imshow('face', face)
-        print(resImg)"""
-    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
