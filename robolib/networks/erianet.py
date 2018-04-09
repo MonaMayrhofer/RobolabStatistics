ec1bf2ab9c25a2a5cdaf4d52dca610ebea09a729
Erik Mayrhofer <obyoxar@gmail.com>
2018-04-09 13:26:58 +0200

    WIP on Convolutional

--- robolib/networks/erianet.py
+++ robolib/networks/erianet.py
@@ -4,10 +4,12 @@ import numpy as np
 from keras.optimizers import RMSprop
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential, Model, load_model
-from keras.layers import Input, Dense, Dropout, Lambda
-from robolib.datamanager.siamese_data_loader import gen_data_new, load_one_image
+from keras.layers import Input, Dense, Dropout, Lambda, Conv2D
+from robolib.datamanager.siamese_data_loader import load_one_image
 from robolib.networks.common import contrastive_loss, euclidean_dist_output_shape, euclidean_distance
+from robolib.util.random import random_different_numbers
 from keras import backend
+from robolib.images.pgmtools import read_pgm
 
 
 class Erianet:
@@ -15,6 +17,8 @@ class Erianet:
         self.input_image_size = input_image_size
         self.input_to_output_stride = input_to_output_stride
         self.model = None
+        self.input_dim = (int(input_image_size[0] / input_to_output_stride), int(input_image_size[1] / input_to_output_stride))
+        # self.input_dim = (int(input_image_size[0] / input_to_output_stride) * int(input_image_size[1] / input_to_output_stride), )
         if not dont_init:
             if modelpath is None or not path.isfile(modelpath):
                 self.create(input_image_size, input_to_output_stride)
@@ -28,7 +32,7 @@ class Erianet:
             data_selection = self.__get_names_of(data_folder)
 
         # TODO GEN_DATA_NEW TO HERE
-        x, y = gen_data_new(1000, data_selection, data_folder, self.input_image_size, self.input_to_output_stride)
+        x, y = self.gen_data_new(1000, data_selection, data_folder, self.input_image_size, self.input_to_output_stride)
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_percent)
 
         self.model.fit([x_train[:, 0], x_train[:, 1]], y_train, validation_split=.25, batch_size=128, verbose=2,
@@ -37,8 +41,9 @@ class Erianet:
 
     def create(self, input_image_size=(128, 128), input_to_output_stride=2):
         assert all(np.mod(input_image_size, input_to_output_stride) == (0, 0))
-        self.model = self.__create_erianet((
-            int(input_image_size[0] / input_to_output_stride) * int(input_image_size[1] / input_to_output_stride), 1))
+        self.model = self.create_erianet()
+
+        #self.input_dim = (int(input_image_size[0] / input_to_output_stride) * int(input_image_size[1] / input_to_output_stride), 1)
         rms = RMSprop()
         self.model.compile(loss=contrastive_loss, optimizer=rms)
 
@@ -51,15 +56,14 @@ class Erianet:
         self.model = load_model(modelpath, custom_objects={'contrastive_loss': contrastive_loss, 'backend': backend})
 
     def predict(self, input_img, reference_data_path, candidates=None, give_all=False):
-        input_img = input_img[::self.input_to_output_stride, ::self.input_to_output_stride]
-        input_img = input_img.reshape((1,
-                                       input_img.shape[0] * input_img.shape[1]))
-        input_img = input_img.astype("float32")
+
+        input_img = self.preprocess(input_img)
+
         if candidates is None:
             candidates = self.__get_names_of(reference_data_path)
         probabilities = np.array([], dtype=[('class', int), ('probability', float)])
         for i in range(0, len(candidates)):
-            reference_img = self.load_image(reference_data_path, candidates[i], 1, False)  # TODO  REF IMAGE INDEX FOR
+            reference_img = self.load_image(reference_data_path, candidates[i], 1, False, preprocess=True)  # TODO  REF IMAGE INDEX FOR
             probability = float(self.model.predict([input_img, reference_img]))
             pair = (i, probability)
             probabilities = np.append(probabilities, np.array(pair, dtype=probabilities.dtype))
@@ -83,14 +87,35 @@ class Erianet:
     def __get_names_of(folder):
         return next(os.walk(folder))[1]
 
-    def load_image(self, reference_path, name, img, show=False, stride=None):
+    def load_image(self, reference_path, name, img, show=False, stride=None, preprocess=False):
         if stride is None:
             stride = self.input_to_output_stride
-        return load_one_image(reference_path, name, img, show, stride, True)
+        image = load_one_image(reference_path, name, img, show)
+        if preprocess:
+            image = self.preprocess(image, stride)
+        return image
 
-    @staticmethod
-    def __create_erianet_base(input_d):
+    def preprocess(self, image, stride=None):
+        if stride is None:
+            stride = self.input_to_output_stride
+        image = image[::stride, ::stride]
+        # image = image.reshape(1, image.shape[0] * image.shape[1])
+        image = image.reshape(image.shape[0], image.shape[1])
+        image = image.astype("float32")
+        return image
+
+    def create_erianet_base(self):
+        input_d = np.concatenate((self.input_dim, [1]))
+        print("Creating")
+        print(input_d)
         seq = Sequential()
+        seq.add(Conv2D(filters=9, kernel_size=(3, 3), strides=(2, 2), activation='relu', input_shape=input_d))
+        seq.add(Dense(200, activation='linear', input_shape=input_d))
+        seq.add(Dense(100, activation='linear'))
+        seq.add(Dropout(0.2))
+        seq.add(Dense(50, activation='linear'))
+
+        """
         seq.add(Dense(600, input_shape=(input_d,), activation='linear'))
         seq.add(Dropout(0.2))
         seq.add(Dense(300, activation='linear'))
@@ -99,7 +124,7 @@ class Erianet:
         seq.add(Dense(100, activation='linear'))
         seq.add(Dropout(0.2))
         seq.add(Dense(50, activation='linear'))
-
+        """
         """
         for i in range(len(hidden_layer_size)):
             if i == 0:
@@ -109,14 +134,84 @@ class Erianet:
             seq.add(Dropout(0.2))"""
         return seq
 
-    @staticmethod
-    def __create_erianet(input_dim):
-        input_size = input_dim[0] * input_dim[1]
-        input_a = Input(shape=(input_size,))
-        input_b = Input(shape=(input_size,))
-        base_network = Erianet.__create_erianet_base(input_size)
+    def create_erianet(self):
+        # input_size = self.input_dim[0] * self.input_dim[1]
+        input_d = np.concatenate((self.input_dim, [1]))
+        print(input_d.shape)
+        input_a = Input(shape=input_d)
+        input_b = Input(shape=input_d)
+        base_network = self.create_erianet_base()
         processed_a = base_network(input_a)
         processed_b = base_network(input_b)
         distance = Lambda(euclidean_distance, output_shape=euclidean_dist_output_shape)([processed_a, processed_b])
         model = Model(inputs=[input_a, input_b], outputs=distance)
         return model
+
+    def gen_data_new(self, train_set_size, class_folder_names, pic_dir, input_image_size=(100, 100),
+                     input_to_output_stride=2):
+        if input_image_size[0] % input_to_output_stride != 0 and input_image_size[1] % input_to_output_stride != 0:
+            raise Exception("Input image size must be divisible by the stride")
+        #size_1 = int(input_image_size[0] / input_to_output_stride)
+        #size_2 = int(input_image_size[1] / input_to_output_stride)
+        total_image_length = self.input_dim
+        classes = len(class_folder_names)
+
+        count = 0
+        x_tr_positive = np.zeros(np.concatenate(([train_set_size, 2], total_image_length)))  # Save n pairs of images
+        y_tr_positive = np.zeros([train_set_size, 1])  # Is this pair a positive or a negative
+
+        # Gen Positive Examples
+        for i in range(classes):  # From all classes
+            for j in range(int(train_set_size / classes)):  # Get two different images of the same person
+                ind1, ind2 = random_different_numbers(10)
+
+                im1 = read_pgm(os.getcwd() + '/' + pic_dir + '/' + class_folder_names[i] + '/' + str(ind1 + 1) + '.pgm',
+                               'rw+')
+                im2 = read_pgm(os.getcwd() + '/' + pic_dir + '/' + class_folder_names[i] + '/' + str(ind2 + 1) + '.pgm',
+                               'rw+')
+
+                im1 = self.preprocess(im1)
+                im2 = self.preprocess(im2)
+
+                #im1 = im1[::input_to_output_stride, ::input_to_output_stride]
+                #im2 = im2[::input_to_output_stride, ::input_to_output_stride]
+
+                #im1 = im1.reshape(total_image_length)
+                #im2 = im2.reshape(total_image_length)
+
+                x_tr_positive[count, 0, :] = im1
+                x_tr_positive[count, 1, :] = im2
+                y_tr_positive[count] = 1
+                count += 1
+
+        count = 0
+        x_tr_negative = np.zeros([train_set_size, 2, total_image_length])
+        y_tr_negative = np.zeros([train_set_size, 1])
+        # Gen Negative Examples
+        for i in range(int(train_set_size / 10)):  # Für ein Zehntel der Testdaten
+            for j in range(10):  # Jeweils 10 Bilder auswählen
+                ind1, ind2 = random_different_numbers(classes)
+
+                im1 = read_pgm(os.getcwd() + '/' + pic_dir + '/' + class_folder_names[ind1] + '/' + str(j + 1) + '.pgm',
+                               'rw+')
+                im2 = read_pgm(os.getcwd() + '/' + pic_dir + '/' + class_folder_names[ind2] + '/' + str(j + 1) + '.pgm',
+                               'rw+')
+
+                im1 = self.preprocess(im1)
+                im2 = self.preprocess(im2)
+
+                #im1 = im1[::input_to_output_stride, ::input_to_output_stride]
+                #im2 = im2[::input_to_output_stride, ::input_to_output_stride]
+
+                #im1 = im1.reshape(im1.shape[0] * im1.shape[1])
+                #im2 = im2.reshape(im2.shape[0] * im2.shape[1])
+
+                x_tr_negative[count, 0, :] = im1
+                x_tr_negative[count, 1, :] = im2
+                y_tr_negative[count] = 0
+                count += 1
+
+        x_train = np.concatenate([x_tr_positive, x_tr_negative], axis=0) / 255  # Squish training-data from 0-255 to 0-1
+        y_train = np.concatenate([y_tr_positive, y_tr_negative], axis=0)
+
+        return x_train, y_train
