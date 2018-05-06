fbd8101f64583979b58d304e72e838f0574374a5
Obyoxar <obyoxar@gmail.com>
2018-05-06 15:18:22 +0200

    Fixed bugs in common.py
    * Fixed Format
    * Fixed euclidean_dist_output to be always accurate

--- robolib/networks/common.py
+++ robolib/networks/common.py
@@ -3,16 +3,15 @@ from keras import backend
 
 def contrastive_loss(y_true, y_pred):
     margin = 5
-    return backend.mean(
-         y_true * backend.square(y_pred) +
-         (1 - y_true) * backend.square(backend.maximum(margin - y_pred, (y_pred-margin)/10)))
+    return backend.mean(y_true * backend.square(y_pred) + \
+                        (1 - y_true) * backend.square(backend.maximum(margin - y_pred, (y_pred - margin) / 10)))
 
 
 def contrastive_loss_manual(correct, prediction):
     if correct:
-        return prediction**2
+        return prediction ** 2
     else:
-        return max(100-prediction, (prediction-100)/10)**2
+        return max(100 - prediction, (prediction - 100) / 10) ** 2
 
 
 def euclidean_distance(vectors):
@@ -22,4 +21,5 @@ def euclidean_distance(vectors):
 
 def euclidean_dist_output_shape(shapes):
     shape1, shape2 = shapes
-    return shape1[0], 1
+    assert shape1[0] == shape2[0]
+    return 1, 1
