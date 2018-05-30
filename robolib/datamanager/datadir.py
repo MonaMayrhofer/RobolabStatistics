5d31653317625ea9c5d87a86b39ed7d62876e1fb
Obyoxar <obyoxar@gmail.com>
2018-05-30 18:14:36 +0200

    Upgraded converter.py

--- robolib/datamanager/datadir.py
+++ robolib/datamanager/datadir.py
@@ -4,6 +4,8 @@ __ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.join(os.path.a
 __DATA_DIR = os.path.join(__ROOT_DIR, 'data')
 __MODEL_DIR = os.path.join(__DATA_DIR, 'models')
 __CONVERTED_IMG_DIR = os.path.join(__DATA_DIR, 'converted')
+__UNCONVERTED_IMG_DIR = os.path.join(__DATA_DIR, 'unconverted')
+__OPENCV_DIR = os.path.join(__DATA_DIR, 'opencv')
 
 
 def converted_image_dir():
@@ -15,4 +17,16 @@ def paparazzi_output(name: str, group: str = 'Images'):
 
 
 def get_model_dir(name):
-    return os.path.join(__MODEL_DIR, name);
+    return os.path.join(__MODEL_DIR, name)
+
+
+def get_unconverted_dir(name):
+    return os.path.join(__UNCONVERTED_IMG_DIR, name)
+
+
+def get_converted_dir(name):
+    return os.path.join(__CONVERTED_IMG_DIR, name)
+
+
+def get_opencv_dir(name):
+    return os.path.join(__OPENCV_DIR, name)
